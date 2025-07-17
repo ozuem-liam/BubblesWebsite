0ffc55c6e64d869d092b0f9f4038a68c8aef60cf
ozuem-liam <ozuemdw@gmail.com>
2025-07-17 08:34:40 +0100

    update fixes

--- app/dashboard/shops/page.tsx
+++ app/dashboard/shops/page.tsx
@@ -1,28 +1,28 @@
-// import { Metadata } from 'next'
-// import { Text } from '@/components/global/Text'
+import { Metadata } from 'next'
+import { Text } from '@/components/global/Text'
 
-// export const metadata: Metadata = {
-//   title: 'Shops | Bubbles',
-// }
+export const metadata: Metadata = {
+  title: 'Shops | Bubbles',
+}
 
-// export default async function ShopPage() {
+export default async function ShopPage() {
 
-//   return (
-//     <section className="h-screen py-4">
-//       <header className='flex flex-wrap justify-between items-center mb-8 gap-4'>
-//         <div>
-//           <Text
-//             as='h2'
-//             style='text-gray-900 text-2xl font-bold mb-2'
-//             children='Featured Shops'
-//           />
-//           <Text
-//             as='p'
-//             style='text-gray-600'
-//             children='Discover top-rated shops in your area'
-//           />
-//         </div>
-//       </header>
-//     </section>
-//   )
-// }
+  return (
+    <section className="h-screen py-4">
+      <header className='flex flex-wrap justify-between items-center mb-8 gap-4'>
+        <div>
+          <Text
+            as='h2'
+            style='text-gray-900 text-2xl font-bold mb-2'
+            children='Featured Shops'
+          />
+          <Text
+            as='p'
+            style='text-gray-600'
+            children='Discover top-rated shops in your area'
+          />
+        </div>
+      </header>
+    </section>
+  )
+}
